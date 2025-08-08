596ea459161a8adc4fa9c25f91df7dd64218b0f6
Jean <jeancarlosfeliz0@gmail.com>
2025-08-07 22:24:03 -0400

    Agregue las funciones agregar amigo y mostrar nombre de amigo en el archivo app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+let listaAmigos = [];
+let nombreAmigo;
+function agregarAmigo(){
+    nombreAmigo = document.getElementById("amigo").value;
+    if (nombreAmigo != ""){
+        listaAmigos.push(nombreAmigo);
+        document.getElementById("amigo").value = '';mostrarNombreAmigo();
+    }else{
+        alert("Ingrese un nombre válido");
+    }
+}
+
+
+function mostrarNombreAmigo(){
+    let lista = document.getElementById("listaAmigos");
+    let elemento = document.createElement("li");
+    elemento.textContent = nombreAmigo;
+    lista.appendChild(elemento);
+
+}
+
+
+
+function sortearAmigo(){
+
+}
